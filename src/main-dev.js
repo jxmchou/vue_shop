11564126658076030b2d1253d49b78734c066cd2
MC猴 <89851851+jxmchou@users.noreply.github.com>
2021-12-31 13:35:53 +0800

    Add files via upload

--- src/main-dev.js
+++ src/main-dev.js
@@ -21,7 +21,7 @@ import 'nprogress/nprogress.css'
 // 导入axios
 import axios from 'axios'
 // 配置请求的根路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
 // 在 request 拦截器中，展示进度条 NProgress.start()
 axios.interceptors.request.use((config) => {
   NProgress.start()
